d54070783c18a2140f3ba2caf063e5d0355aa453
Haiquan Zheng <hqzktb@gmail.com>
2021-06-29 20:07:19 -0500

    deleted unuseful directory

--- otherPages/about.js
+++
@@ -1,9 +0,0 @@
-let home = document.querySelector("#home");
-let myName = document.querySelector("#my-name");
-home.addEventListener("mouseover", function () {
-  myName.innerText = "Go Back to Home";
-});
-
-home.addEventListener("mouseout", function () {
-  myName.innerText = "Haiquan Patrick";
-});
